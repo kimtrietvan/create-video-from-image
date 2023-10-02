b698afe5b45abcd39fdc1825dcc181cc8f61eb85
Trường Đinh <truongdinh@Truongs-MacBook-Pro.local>
2023-10-03 02:45:21 +0700

    :v

--- main.py
+++
@@ -1,7 +0,0 @@
-from modules.Image import Image
-
-im = Image()
-
-im.loadFromURL('https://previews.123rf.com/images/kchung/kchung1610/kchung161001354/64508202-test-written-by-hand-hand-writing-on-transparent-board-photo.jpg')
-
-im.showImage()
